be9949e8e78057c0568e61e33ad841097c6bcc6a
fmasz6 <fmasz6@gmail.com>
2025-03-24 20:32:16 -0400

    Clerk Auth Updated Image Centered

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
@@ -6,6 +7,8 @@ export default function Home() {
     <div>
       <h1>Subscribe to Tubeguruji</h1>
       <Button>Hello</Button>
+      
+      <UserButton/>
     </div>
   );
 }
