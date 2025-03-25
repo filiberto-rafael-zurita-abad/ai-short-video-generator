3bbfe58cfd58944dc252275b93ee69be2e3b7f77
fmasz6 <fmasz6@gmail.com>
2025-03-24 20:50:22 -0400

    User Auth Connected to Neon db

--- app/provider.js
+++ app/provider.js
@@ -1,6 +1,33 @@
-import React from 'react'
+"use client"
+import { db } from '@/configs/db';
+import { Users } from '@/configs/schema';
+import { useUser } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm';
+import React, { useEffect } from 'react'
 
 function Provider({children}) {
+
+  const {user}=useUser();
+
+  useEffect(()=>{
+    user&&isNewUser();
+  }, [user]);
+
+  const isNewUser=async()=>{
+    const result=await db.select().from(Users)
+    .where(eq(Users.email,user?.primaryEmailAddress?.emailAddress));
+
+    if(!result[0])
+    {
+      await db.insert(Users).values({
+        name:user.fullName,
+        email:user?.primaryEmailAddress?.emailAddress,
+        imageUel:user?.imageUrl
+      })
+    }
+
+  }
+
   return (
     <div>
       {children}
