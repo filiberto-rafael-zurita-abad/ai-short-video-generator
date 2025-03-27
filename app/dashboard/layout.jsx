fa31024c75342991c1962555dad6cd78acca6068
fmasz6 <fmasz6@gmail.com>
2025-03-27 11:20:51 -0400

    Updated dashboard

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+export default function DashboardLayout({
+  children,
+}) {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-4">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
