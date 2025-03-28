1a399b97fe42c52ebf19ac20223e5634e49d261c
fmasz6 <fmasz6@gmail.com>
2025-03-27 20:45:59 -0400

    Dashboard cards updated magins

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 import Card from "@/components/ui/Card";
 import { cardsData } from "./(data)/cardsData";
 
--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -3,9 +3,9 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Card({ title, content, buttonText }) {
   return (
-    <div className="border rounded-md p-4">
-      <h1>{title}</h1>
-      <p>{content}</p>
+    <div className="border rounded-md p-4 w-80 h-56">
+      <h1 className="truncate mb-4">{title}</h1>
+      <p className="overflow-hidden text-ellipsis whitespace-normal h-26">{content}</p>
       <Button>{buttonText}</Button>
       <UserButton />
     </div>
