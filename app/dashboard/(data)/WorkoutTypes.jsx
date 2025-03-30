3a7eb2db4b1c8ea3d2aa7bba4a60a81521a501bb
fmasz6 <fmasz6@gmail.com>
2025-03-30 19:49:46 -0400

    Date-Time Field Updated

--- app/dashboard/(data)/WorkoutTypes.jsx
+++ app/dashboard/(data)/WorkoutTypes.jsx
@@ -1,11 +1,11 @@
 const workoutTypes = {
-    headers: ["Id","User", "Name", "Weight (kg)", "Weight (lb)", "CPR"],
+    headers: ["Id","User", "Date-Time", "Name", "Weight (kg)", "Weight (lb)", "CPR"],
     rows: [
-      [1, "fmasz6@gmail.com", "Warm-up", 0, 0, 2],
-      [2, "fmasz6@gmail.com", "Legs", 30, 66.14, 1],
-      [3, "fmasz6@gmail.com", "Back", 80, 179.37, 1],
-      [4, "fmasz6@gmail.com", "Chest", 30, 66.14, 0.5],
-      [5, "fmasz6@gmail.com", "Gluts", 25, 55.12, 0.5],
+      [1, "fmasz6@gmail.com", 0, "Warm-up", 0, 0, 2],
+      [2, "fmasz6@gmail.com", 0, "Legs", 30, 66.14, 1],
+      [3, "fmasz6@gmail.com", 0, "Back", 80, 179.37, 1],
+      [4, "fmasz6@gmail.com", 0, "Chest", 30, 66.14, 0.5],
+      [5, "fmasz6@gmail.com", 0, "Gluts", 25, 55.12, 0.5],
     ],
   };
   
